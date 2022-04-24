6d2b80f6a2758d4071ba3de582f369ecb01cd45d
Jake McInerney <jakemcinerney1@gmail.com>
2022-04-23 22:25:55 -0500

    finished POST route in router and client

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -6,14 +6,14 @@ const pool = require('../modules/pool');
 // Get all tasks from database
 
 tasksRouter.get('/', (req, res) => {
-  let queryText = 'SELECT * FROM "tasks" ORDER BY "importance";';
+  let sqlQuery = 'SELECT * FROM "tasks" ORDER BY "importance";';
   pool
-    .query(queryText)
+    .query(sqlQuery)
     .then((result) => {
       res.send(result.rows);
     })
     .catch((error) => {
-      console.log('Error getting tasks', error);
+      console.log('Error in GET /tasks db request:', error);
       res.sendStatus(500);
     });
 });
@@ -21,8 +21,25 @@ tasksRouter.get('/', (req, res) => {
 // Add task to database
 
 tasksRouter.post('/', (req, res) => {
-   
-})
+  let newTask = req.body;
+  console.log('Adding task', newTask);
+
+  let sqlQuery = `
+   INSERT INTO "tasks"
+    ("name", "type", "importance", "notes")
+    VALUES
+    ($1, $2, $3, $4);
+   `;
+  let sqlValues = [req.body.name, req.body.type, req.body.importance, req.body.notes];
+  pool
+    .query(sqlQuery, sqlValues)
+    .then((dbResult) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('Error in POST /tasks db request:', error);
+    });
+});
 
 // Update task (complete = true or false) on database
 
